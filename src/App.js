37be8017e45da84fa37a4485090e298c95491cc8
Safayat Hossain <safayathossain40@gmail.com>
2024-04-12 22:59:50 +0600

    Country componenet added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import Country from './components/Country/Country';
 
 function App() {
+  const[countries,setCountries] = useState([]);
+  useEffect(()=>{
+    fetch('https://restcountries.com/v3.1/all')
+    .then(res => res.json())
+    .then(data => setCountries(data))
+    .catch(error => console.log(error))
+  },[]);
+
   return (
     <div className="App">
+      <h1>country loaded : {countries.length} </h1>
+      <ul>
+        {
+          countries.map(country => <Country name={country.name}>
+            {country.name}
+          </Country>)
+        }
+      </ul>
       <header className="App-header">
-        <h1>ultra nation</h1>
-        
       </header>
     </div>
   );
 }
+
+
 
 export default App;
